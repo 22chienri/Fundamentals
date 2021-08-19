b2b9dc545bea579f47c96452f4a00b7faab41241
Campionnn <awesomelaptop@gmail.com>
2021-08-19 10:51:19 -0700

    Added User Input to CompareInts

--- src/CompareInts.java
+++ src/CompareInts.java
@@ -1,11 +1,15 @@
 import java.text.MessageFormat;
+import java.util.Scanner;
 
 public class CompareInts
 {
     public static void main(String[] args)
     {
-        int a = 10;
-        int b = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter value for a:");
+        double a = Double.parseDouble(input.nextLine());
+        System.out.println("Enter value for b:");
+        double b = Double.parseDouble(input.nextLine());
         if (a == b)
         {
             System.out.println(MessageFormat.format("The value of a is {0} and it is equal to b whose value is {1}.", a, b));
