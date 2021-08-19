85cc42796a0df7ffe735e0cd7fdac414546b8701
Campionnn <awesomelaptop@gmail.com>
2021-08-19 11:31:36 -0700

    fixed comments and changed to System.out.printf

--- src/CompareInts.java
+++ src/CompareInts.java
@@ -1,24 +1,34 @@
-import java.text.MessageFormat;
+/*
+Ryan Chien
+Period 4
+Fundamentals
+CompareInts
+ */
+
 import java.util.Scanner;
 
-public class CompareInts
-{
-    public static void main(String[] args)
-    {
+public class CompareInts {
+    public static void main(String[] args) {
+        // create scanner object to get user input
         Scanner input = new Scanner(System.in);
+        // get user input and assign to a and b
         System.out.println("Enter value for a:");
-        double a = Double.parseDouble(input.nextLine());
+        int a = Integer.parseInt(input.nextLine());
         System.out.println("Enter value for b:");
-        double b = Double.parseDouble(input.nextLine());
-        if (a == b)
-        {
-            System.out.println(MessageFormat.format("The value of a is {0} and it is equal to b whose value is {1}.", a, b));
-        } else if (a < b)
-        {
-            System.out.println(MessageFormat.format("The value of a is {0} and it is less than b whose value is {1}.", a, b));
-        } else if (a > b)
-        {
-            System.out.println(MessageFormat.format("The value of a is {0} and it is greater than b whose value is {1}.", a, b));
+        int b = Integer.parseInt(input.nextLine());
+
+        // determine which case it is
+        if (a == b) {
+            // print output with a and b in the string
+            System.out.printf("The value of a is %s and it is equal to b whose value is %s.%n", a, b);
+        }
+        else if (a < b) {
+            // print output with a and b in the string
+            System.out.printf("The value of a is %s and it is less than b whose value is %s.%n", a, b);
+        }
+        else if (a > b) {
+            // print output with a and b in the string
+            System.out.printf("The value of a is %s and it is greater than b whose value is %s.%n", a, b);
         }
     }
 }
--- src/MyFavorite.java
+++ src/MyFavorite.java
@@ -1,7 +1,13 @@
-public class MyFavorite
-{
-    public static void main(String[] args)
-    {
+/*
+Ryan Chien
+Period 4
+Fundamentals
+MyFavorite
+ */
+
+public class MyFavorite {
+    public static void main(String[] args) {
+        // print all favorites on new lines
         System.out.println("My favorite food is: Pizza");
         System.out.println("My favorite color is: Red");
         System.out.println("My favorite movie is: Pirates of the Caribbean");
