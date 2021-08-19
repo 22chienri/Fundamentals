110b61e80070a765024c54ac178e6cea3119a350
Campionnn <awesomelaptop@gmail.com>
2021-08-19 10:31:32 -0700

    Initial commit

---
+++ src/MyFavorite.java
@@ -0,0 +1,10 @@
+public class MyFavorite
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My favorite food is: Pizza");
+        System.out.println("My favorite color is: Red");
+        System.out.println("My favorite movie is: Pirates of the Caribbean");
+        System.out.println("My favorite musical group is: Queen");
+    }
+}
