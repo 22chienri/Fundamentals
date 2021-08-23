854297f0b3993aeda48395c66118d628944308aa
Campionnn <awesomelaptop@gmail.com>
2021-08-23 12:15:48 -0700

    changed to use Math.random() instead of Random

--- src/RandNums.java
+++ src/RandNums.java
@@ -5,23 +5,13 @@ Fundamentals
 RandNums
  */
 
-import java.util.Random;
-import java.util.Scanner;
 
 public class RandNums {
     public static void main(String[] args) {
-        // create scanner object to get user input
-        Scanner input = new Scanner(System.in);
-        // create random object to generate random number
-        Random random = new Random();
-        // get user input and assign to n
-        System.out.println("How many interations?");
-        int n = Integer.parseInt(input.nextLine());
-
-        for (int i = 1; i <= n; i++) {
-
-            // print final result
-            System.out.printf("%s\t%s\t%s%n", i, i * i, random.nextInt(i * i - i + 1) + i);
+        // loop 10 times with index
+        for (int i = 1; i <= 10; i++) {
+            // print result
+            System.out.printf("%s\t%s\t%s%n", i, i * i, i + (int)(Math.random() * ((i * i - i) + 1)));
         }
     }
 }
