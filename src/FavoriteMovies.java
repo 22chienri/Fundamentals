fc78e19ccfc53c43a6cf424ebd3f24527bea39ad
Campionnn <awesomelaptop@gmail.com>
2021-08-23 11:46:07 -0700

    finished FavoriteMovies

---
+++ src/FavoriteMovies.java
@@ -0,0 +1,34 @@
+/*
+Ryan Chien
+Period 4
+Fundamentals
+FavoriteMovies
+ */
+
+
+public class FavoriteMovies {
+    public static void main(String[] args) {
+        // define arrays
+        String[] favMovies = {"Infinity War", "Guardians of the Galaxy", "Pirates of the Caribbean"};
+        String[] favSongs = {"Bohemian Rhapsody", "Mr. Blue Sky", "Come Sail Away"};
+
+        // print movies on separate lines
+        for (String i : favMovies) {
+            System.out.println(i);
+        }
+        // spacing
+        System.out.print("\n\n");
+        // interatino integer
+        int i = 0;
+        // print songs on same line with commas
+        while (i < favSongs.length) {
+            System.out.print(favSongs[i]);
+            // print comma if not the last item
+            if (i < favSongs.length - 1) {
+                System.out.print(", ");
+            }
+            // increment i by 1
+            i++;
+        }
+    }
+}
