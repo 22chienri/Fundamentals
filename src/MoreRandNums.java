aa2030e082ef50dd19af7bb498297880ef300aa2
Campionnn <awesomelaptop@gmail.com>
2021-08-24 10:48:15 -0700

    changed to use random instead of math.random

--- src/MoreRandNums.java
+++ src/MoreRandNums.java
@@ -5,13 +5,17 @@ Fundamentals
 MoreRandNums
  */
 
+import java.util.Random;
+
 public class MoreRandNums {
     public static void main(String[] args) {
+        // create random object
+        Random random = new Random();
         // define array
         int[] array = new int[50];
         // fill array with random numbers between 0 and 100
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int)(Math.random() * (100 + 1));
+            array[i] = random.nextInt(100);
         }
         // print each number in the array with a space between
         for (int num : array) {
